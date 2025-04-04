03c83572b0ab17774a9102f52c086d1484465512
drogariagloboti <drogariaglobo.ti@gmail.com>
2025-04-04 17:48:05 -0300

    feat: integrate Tailwind CSS and update styles across components
    - Added Tailwind CSS dependencies in package.json.
    - Updated button component with Tailwind classes for styling.
    - Enhanced step column component layout and styling using Tailwind.
    - Modified home layout background to a gradient and adjusted styles.
    - Increased project name font size and added gradient background in home component.
    - Refactored home component HTML structure for better readability and styling.
    - Updated environment configuration for local development.
    - Added Google Fonts preconnect and Inter font in index.html.
    - Imported Tailwind CSS in global styles and changed default font to Inter.
    - Created Tailwind CSS configuration file for project.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Processa todos os arquivos HTML e TypeScript no Angular
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
