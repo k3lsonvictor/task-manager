c941ce3c9520a7ee1efc403f9ed64d2577032b27
drogariagloboti <drogariaglobo.ti@gmail.com>
2025-04-14 13:34:48 -0300

    feat: update button component to accept custom class for styling

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -9,4 +9,10 @@ import { Component, Input } from '@angular/core';
 export class ButtonComponent {
   @Input() clickFunction: (() => void) | null = null;
   @Input() content!: string;
+  @Input() customClass: string = '';
+
+  ngOnInit() {
+    console.log('Button component initialized with content:', this.content);
+    console.log('Button component initialized with class:', this.customClass);
+  }
 }
