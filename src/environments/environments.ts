d3f9c27d0f540bd1cf81f02a810f8854d006e75f
k3lsonvictor <k3lson.victor@gmail.com>
2025-05-24 21:43:48 -0300

    feat: update button component for cursor pointer and enhance detail card modal description styling

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  BASE_URL: 'http://localhost:3000'
+  // BASE_URL: 'http://localhost:3000'
   // BASE_URL: 'http://10.11.1.5:3000'
-  // BASE_URL: "https://task-manager-api-2-ltk2.onrender.com"
+  BASE_URL: "https://task-manager-api-1-ym8c.onrender.com"
 };
